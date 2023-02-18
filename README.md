6a3edcaf8b74dd33d9719922b9a6e4ece3b0a6ab
VictorXypCh <victorxyp.ch@gmail.com>
2023-02-18 16:08:29 +0700

    Cypress added e2e test, done part5.d

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # FullStackOpen Frontend
-
--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -1,6 +1,13 @@
 describe('Note app', function () {
   beforeEach(function () {
-    cy.visit('http://localhost:3000')
+    cy.visit('')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen',
+    }
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
   })
 
   it('front page can be opened', function () {
@@ -20,11 +27,69 @@ describe('Note app', function () {
   })
 
   it('user can login', function () {
-          cy.contains('log in').click()
+    cy.contains('log in').click()
 
     cy.get('#username').type('mluukkai')
     cy.get('#password').type('salainen')
     cy.get('#login-button').click()
     cy.contains('Matti Luukkainen logged in')
   })
+
+  describe('when logged in', function () {
+    beforeEach(function () {
+      cy.login({ username: 'mluukkai', password: 'salainen' })
+    })
+
+    it('a new note can be created', function () {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+
+    describe('and a note exist', function () {
+      beforeEach(function () {
+        cy.contains('new note').click()
+        cy.get('input').type('another note cypress')
+        cy.contains('save').click()
+      })
+
+      it('can make note important', function () {
+        cy.contains('another note cypress')
+          .parent()
+          .contains('make not important')
+          .click()
+
+        cy.contains('another note cypress').parent().contains('make important')
+      })
+    })
+
+    describe('and several notes exist', function () {
+      beforeEach(function () {
+        cy.createNote({ content: 'first note', important: false })
+        cy.createNote({ content: 'second note', important: false })
+        cy.createNote({ content: 'third note', important: false })
+      })
+
+      it('one of those can be made important', function () {
+        cy.contains('second note').parent().find('button').as('theButton')
+        cy.get('@theButton').click()
+        cy.get('@theButton').should('contain', 'make not important')
+      })
+    })
+  })
+
+  //it.only('login fails with wrong password', function () {
+  //  cy.contains('log in').click()
+  //  cy.get('#username').type('mluukkai')
+  //  cy.get('#password').type('wrong')
+  //  cy.get('#login-button').click()
+
+  //  cy.get('.error')
+  //    .should('contain', 'Wrong credentials')
+  //    .and('have.css', 'color', 'rgb(255, 0, 0)')
+  //    .and('have.css', 'border-style', 'solid')
+
+  //  cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
+  //})
 })
